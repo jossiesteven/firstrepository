f6ee253d8b912c60401c1232fb34f6029c3ee057
jossiesteven <jossiesteven@gmail.com>
2023-03-21 22:53:15 -0500

    poo

---
+++ poo/index.php
@@ -0,0 +1,39 @@
+<?php
+echo "<h1>Hola Mundo en PHP</h1>";
+
+$fecha = "22 Marzo";
+
+echo $fecha;
+
+$fecha = 12;
+
+if ($fecha == 12){
+    echo "<br>".$fecha;
+}
+
+for($i=0; $i<10; $i++){
+    echo "<br>",$i;
+}
+
+// while ($a<=10){
+//     ...
+// }
+
+// do {
+//     # code....
+//     }
+// while($a<=10){
+
+// }
+
+// switch ($variable){
+
+//     case "value":
+//         # code...
+//         break;
+//     default:
+//     # code...
+//     break;
+// }
+
+?>
