06ec7c37d38558e3eb94bbfa66218de26ec07c43
jossiesteven <jossiesteven@gmail.com>
2023-06-17 14:53:44 -0500

    sql

---
+++ scriptSQL/update-delete-select-where.sql
@@ -0,0 +1,88 @@
+/*Actualización y eliminación registros*/
+
+select *
+from empleados;
+
+/*Actualización*/
+
+update empleados 
+set salario=1200000;
+
+update empleados 
+set salario=800000, edad=20
+where id=6;
+
+update empleados 
+set salario=1000000, edad=60
+where nombre'Carlos';
+
+
+update empleados 
+set salario=1100
+where id=6;
+
+
+update empleados 
+set salario=3300
+where nombre='Carlos' and edad=18;
+
+
+/*Eliminación*/
+
+delete from empleados 
+where id=5;
+
+delete from empleados 
+where id=7;
+
+delete from empleados 
+where nombre='gloria';
+
+delete from empleados 
+where nombre='carlos' and edad=60;
+
+/*Consultas compuestas*/
+
+select * 
+from empleados 
+where nombre='juan';
+
+select * 
+from empleados 
+where edad<30;
+
+
+select * 
+from empleados 
+where edad>30;
+
+select * 
+from empleados 
+where edad>30 and salario<1000000;
+
+select * 
+from empleados 
+where edad<30 and salario>1000000;
+
+select nombre, salario
+from empleados e where salario is null;
+
+select nombre, edad
+from empleados 
+where edad<>18;
+
+select * 
+from empleados 
+where edad>30 or salario<1000000;
+
+
+
+
+
+/*>,<,>,<>*/
+
+
+
+
+
+
